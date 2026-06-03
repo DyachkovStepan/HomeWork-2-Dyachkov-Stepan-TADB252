7e2ea6702dec270b7c0a7e35901836dabaf071b5
Дьячков Степан <sndyachkov@edu.hse.ru>
2026-06-03 21:53:53 +0300

    feat: В файл classes.py добавлен класс Recipe

--- classes.py
+++ classes.py
@@ -23,3 +23,34 @@ class Ingredient:
     
     def __eq__(self, other):
         return (other.name == self.name) and (other.unit == self.unit)
+
+# Ваш код здесь
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if  i.name == ingredient.name and i.unit == ingredient.unit:
+                i.quantity += ingredient.quantity
+                break
+        else:
+            self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if isinstance(ratio, (int,float)) and ratio > 0:
+            return True
+        return False
+    
+    def scale(self,ratio):
+        ingredients = [Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit) for ingredient in self.ingredients]
+        return Recipe(self.title, ingredients)
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        ingredients_sup = "\n".join(str(ingredient) for ingredient in self.ingredients)
+        return f"{self.title} состоит из ингредиентов: \n{ingredients_sup}"
